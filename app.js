62fad3d9f1c8a88772d9bd04c12509b3897d48ba
Simulacrum <tohu@localhost.localdomain>
2019-01-08 22:32:28 -0200

    Use switch case statements instead of bulky else ifs

--- app.js
+++ app.js
@@ -15,70 +15,83 @@ client.on('message', msg => {
     var output;
     var commandName;
     var link = false; // To determine if output is a link
-    if (msg.content === "!help") { // Help Menu
-    const embed = new Discord.RichEmbed()
-        .setTitle("List of Commands")
-        .addField('!b64e or !b64d', 'Base 64 Encoding or Decoding', true)
-        .addField('!b32e or !b32d', 'Base32 Encoding or Decoding', true)
-        .addField('!MoEn or !MoDe', 'Morse Code Encoding or Decoding', true)
-        .addField('!biEn or biDe', 'Binary Encoding or Decoding', true)
-        .addField('!ASCI or !ASCT', 'Text to ASCII Decimal or vice versa', true)
-        .addField('!Anag', 'Finding English Anagrams', true)
-        .setColor('#2976f2');
-    msg.channel.send(embed);
-    } else if (command === "!b64e") { // Base64 Encoding
-        commandName = "Base64 Encoding";
-        output = btoa(content);
-    } else if (command === "!b64d") { // Base64 Decoding
-        commandName = "Base64 Decoding";
-        output = atob(content);
-    } else if (command === "!b32e") { // Base32 Encoding
-        commandName = "Base32 Encoding";
-        output = base32.encode(content);
-    } else if (command === "!b32d") { // Base32 Decoding
-        commandName = "Base32 Decoding";
-        output = base32.decode(content);
-    } else if (command === "!moen") { // Morse Encoding MoEn
-        commandName = "Morse Encoding";
-        output = morse.encode(content);
-    } else if (command === "!mode") { // Morse Decoding MoDe
-        commandName = "Morse Decoding";
-        output = morse.decode(content);
-    } else if (command === "!anag") { // Anagrams Anag
-        commandName = "Finding Anagrams";
-        link = true;
-        output = encodeUrl("https://anagram-solver.net/" + content);
-    } else if (command === "!asci") { // Text to ASCII Decimal Value ASCI
-        commandName = "Text to ASCII Decimal Value";
-        var arr = content.split("");
-        var hexArr = [];
-        for (var i = 0; i < arr.length; i++) {
-            hexArr.push(arr[i].charCodeAt(0));
-        }
-        output = hexArr.join(" ");
-    } else if (command === "!asct") { // ASCII Decimal to Text
-        commandName = "Text to ASCII Decimal Value";
-        var arr = content.split(" ");
-        var hexArr = [];
-        for (var i = 0; i < arr.length; i++) {
-            hexArr.push(String.fromCharCode(arr[i]));
-        }
-        output = hexArr.join("");
-    } else if (command === "!bien") { // Binary Encoding biEn
-        commandName = "Binary Encoding";
-        output = content.trim()  
-        .split("")
-        .map(item => ("0000000" + item.charCodeAt().toString(2))
-          .split("").slice(-8).join(""))
-        .join(" ");
-    } else if (command === "!bide") { // Binary Decoding biDe
-        commandName = "Binary Decoding";
-        output = content.trim().split(" ")  
-        .map(item => String.fromCharCode(parseInt(item, 2)))
-        .join("");
+    switch(command) {
+	case "!help":  // Help Menu
+    		const embed = new Discord.RichEmbed()
+        		.setTitle("List of Commands")
+        		.addField('!b64e or !b64d', 'Base 64 Encoding or Decoding', true)
+        		.addField('!b32e or !b32d', 'Base32 Encoding or Decoding', true)
+        		.addField('!MoEn or !MoDe', 'Morse Code Encoding or Decoding', true)
+        		.addField('!biEn or biDe', 'Binary Encoding or Decoding', true)
+        		.addField('!ASCI or !ASCT', 'Text to ASCII Decimal or vice versa', true)
+        		.addField('!Anag', 'Finding English Anagrams', true)
+        		.setColor('#2976f2');
+   		 msg.channel.send(embed);
+		 break;
+	case "!b64e":  // Base64 Encoding
+	        commandName = "Base64 Encoding";
+		output = btoa(content);
+		break;
+	case "!b64d":  // Base64 Decoding
+	        commandName = "Base64 Decoding";
+	        output = atob(content);
+		break;
+	case "!b32e":  // Base32 Encoding
+	        commandName = "Base32 Encoding";
+	        output = base32.encode(content);
+		break;
+	case "!b32d":  // Base32 Decoding
+	        commandName = "Base32 Decoding";
+	        output = base32.decode(content);
+		break;
+	case "!moen":  // Morse Encoding MoEn
+	        commandName = "Morse Encoding";
+	        output = morse.encode(content);
+		break;
+	case "!mode":  // Morse Decoding MoDe
+	        commandName = "Morse Decoding";
+	        output = morse.decode(content);
+		break;
+	case "!anag":  // Anagrams Anag
+	        commandName = "Finding Anagrams";
+	        link = true;
+	        output = encodeUrl("https://anagram-solver.net/" + content);
+		break;
+	case "!asci":  // Text to ASCII Decimal Value ASCI
+	        commandName = "Text to ASCII Decimal Value";
+	        var arr = content.split("");
+	        var hexArr = [];
+	        for (var i = 0; i < arr.length; i++) {
+	            hexArr.push(arr[i].charCodeAt(0));
+	        }
+	        output = hexArr.join(" ");
+		break;
+	case "!asct":  // ASCII Decimal to Text
+	        commandName = "Text to ASCII Decimal Value";
+	        var arr = content.split(" ");
+	        var hexArr = [];
+	        for (var i = 0; i < arr.length; i++) {
+	            hexArr.push(String.fromCharCode(arr[i]));
+	        }
+	        output = hexArr.join("");
+		break;
+	case "!bien":  // Binary Encoding biEn
+	        commandName = "Binary Encoding";
+	        output = content.trim()
+	        .split("")
+	        .map(item => ("0000000" + item.charCodeAt().toString(2))
+	        .split("").slice(-8).join(""))
+	        .join(" ");
+		break;
+	case "!bide":  // Binary Decoding biDe
+	        commandName = "Binary Decoding";
+	        output = content.trim().split(" ")
+	        .map(item => String.fromCharCode(parseInt(item, 2)))
+	        .join("");
+	   	break;
     }
-    
-    if (output != undefined && link === false) { 
+
+    if (output != undefined && link === false) {
         msg.channel.send("**" + commandName + "** for " + `${msg.author}`);
         msg.channel.send("```" + output + "```");
     } else if (output != undefined && link === true) { // if output is a link
