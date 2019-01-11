f555906d49c793d8d238ea0efc9008d240b2b157
thebrucecgit <brucecccccc@gmail.com>
2019-01-11 23:40:55 +0000

    Replaced Request library with Got

--- cmds/anagram.js
+++ cmds/anagram.js
@@ -1,22 +1,23 @@
 const encodeUrl = require('encodeurl'),
-request = require("request");
+got = require("got");
 
 module.exports = {
   hits: ['anag', 'anagram', 'nag-a-ram'],
   handler: (msg, content, client) => {
     const commandName = "Anagrams";
-    request(encodeUrl("http://anagramica.com/all/" + content), function(err, res, body){
-			if (err) {
-        msg.reply('Critical Request Error');
-				return console.log(err);
-			} else {
-				var anagrams = JSON.parse(body).all.slice(0, 8).join(", \n");
-				const output = anagrams;
-        msg.returnOutput(client, {
-          commandName,
-          output
-        });
-			}
-		});
+    (async () => {
+      try {
+          const response = await got(encodeUrl("http://anagramica.com/all/" + content));
+  				var anagrams = JSON.parse(response.body).all.slice(0, 8).join(", \n");
+  				const output = anagrams;
+          msg.returnOutput(client, {
+            commandName,
+            output
+          });
+      } catch (error) {
+          msg.reply(error.response.body);
+          console.log(error.response.body);
+      }
+    })();
   }
 };
