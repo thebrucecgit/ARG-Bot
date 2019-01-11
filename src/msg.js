125e7a890c6df6ed5137e1cdef0e7aa0e96b4b04
Max Rumsey <22051776+dathappysheep@users.noreply.github.com>
2019-01-11 19:24:04 +0800

    Adding restart command

--- src/msg.js
+++ src/msg.js
@@ -3,11 +3,16 @@ const Discord = require('discord.js')
 module.exports = {
   load: function(msg) {
     msg.authorIsAdmin = authorIsAdmin;
+    msg.authorIsDev = authorIsDev;
     msg.returnOutput = returnOutput;
   }
 }
 function authorIsAdmin() {
   if (config.admins.includes(this.member.id)) return true;
+  return false;
+}
+function authorIsDev() {
+  if (config.devs.includes(this.member.id)) return true;
   return false;
 }
 function returnOutput(msg, result){
