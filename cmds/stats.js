d79339c8cf2aa7e551e129057e0c7073f49f3126
Max Rumsey <22051776+dathappysheep@users.noreply.github.com>
2019-01-11 15:10:56 +0800

    Mass changes.

--- cmds/stats.js
+++ cmds/stats.js
@@ -3,20 +3,20 @@ module.exports = {
   hits: ['stats', "info"],
   handler: (msg, content, client) => {
     const res = {};
-    function uptime() {
-      let totalSeconds = Math.floor(client.uptime / 1000);
-      let days = Math.floor(totalSeconds / 86400);
-      let hours = Math.floor(totalSeconds / 3600);
-      totalSeconds %= 3600;
-      let minutes = Math.floor(totalSeconds / 60);
-      let seconds = totalSeconds % 60;
-      return(`${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`);
-    }
-    
-    if (msg.member.id == 338543472099196928 || msg.member.id == 213928278497558528) {
-      res.output = `ARG-Bot v${version} \nUsers: ${client.users.size} \nChannels: ${client.channels.size} \nGuilds: ${client.guilds.size} \nUptime: ${uptime()}`;
+
+    if (msg.authorIsAdmin()) {
+      res.output = `ARG-Bot v${version} \nUsers: ${client.users.size} \nChannels: ${client.channels.size} \nGuilds: ${client.guilds.size} \nUptime: ${uptime(client)}`;
     }
     res.commandName = "Statistics";
     return res;
   }
 };
+function uptime(client) {
+  let totalSeconds = Math.floor(client.uptime / 1000);
+  let days = Math.floor(totalSeconds / 86400);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  return(`${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`);
+}
