46318b329a5dbbfbb8d318ff6cf7403ee5ef9f13
thebrucecgit <brucecccccc@gmail.com>
2019-01-24 01:10:25 +0000

    Added better error response for Anagram cmd

--- cmds/anagram.js
+++ cmds/anagram.js
@@ -5,20 +5,32 @@ module.exports = {
   hits: ['anag', 'anagram', 'nag-a-ram'],
   handler: (msg, content, client) => {
     const commandName = "Anagrams";
-    (async () => {
-      try {
-          const response = await got(encodeUrl("http://anagramica.com/all/" + content));
-          var anagrams = JSON.parse(response.body).all.slice(0, 8).join(", \n");
-  				const output = anagrams;
-          msg.returnOutput(msg, {
+    if (content.length > 10) {
+      returnOutput("Sorry, your word is longer than 10 characters");
+    } else if (content.match(/^[A-Za-z]+$/)) {
+      (async function getAnag() {
+        try {
+            const response = await got(encodeUrl("http://anagramica.com/best/" + content));
+            if (JSON.parse(response.body).best[0] !== undefined){
+              var anagrams = JSON.parse(response.body).best.slice(0, 8).join(", \n");
+              returnOutput(anagrams);
+            } else {
+              returnOutput("No anagrams were found \nThis does not find multi-word anagrams");
+            }
+        } catch (error) {
+            msg.reply(error.response);
+            console.log(error);
+        }
+      })();
+    } else {
+      returnOutput("Error: Please make sure you only enter alphabet characters");
+    }
+    function returnOutput(output){
+      msg.returnOutput(msg, {
             commandName,
             output,
             input: content
-          });
-      } catch (error) {
-          msg.reply(error.response);
-          console.log(error);
-      }
-    })();
+      });
+    }
   }
 };
